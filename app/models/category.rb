8beed9408ed1d31068a50e0be7612d909a6fc728
Haloren <mckennakyler@gmail.com>
2020-08-06 13:24:53 -0700

    removed category table

--- app/models/category.rb
+++
@@ -1,4 +0,0 @@
-class Category < ApplicationRecord
-    has_many :chores
-	has_many :members, through: :chores
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_171416) do
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2020_08_06_170610) do
 
   create_table "chores", force: :cascade do |t|
     t.string "name"

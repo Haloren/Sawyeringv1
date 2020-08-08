ebfc0b5d9f488fe8289f1035d764623ac34f3d85
Haloren <mckennakyler@gmail.com>
2020-08-07 21:17:53 -0700

    set up helpers in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
 
+    def current_household
+        @current_household ||= Household.find_by_id(session[:household_id]) # if session[:household_id])
+    end
 
+    def logged_in?
+        !!current_household # !!session[:household_id]
+    end
 
+    def nonlogged_in #unauthenticated
+        redirect_to '/login' if !logged_in?
+    end
 
 end
